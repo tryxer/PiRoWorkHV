99e970f4f7eb0284f07591c68c1e6cc7ebab235d
Pierrick LATIMIER <piefreemail@free.fr>
2017-09-18 11:51:43 +0200

    Modif pge Actualités
    - TLC des img
    - re-link img
    - Background body-actualiités

--- wp-content/themes/piro/actualites.php
+++ wp-content/themes/piro/actualites.php
@@ -3,145 +3,115 @@
     include "header.php";
 ?>
 
+<!-- Zone Modifiée le 14/09/17 =========== -->
+        
+        <section class="template-page-actualites">
+            <!-- ACTUALITE PRINCIPALE -->
+            <div class="row">
+                <h2 class="white-text titreSection col s12 offset-l1">Actualités</h2>
+            </div>
+            <!--div class="container"-->
+            <div id="actualitePrincipale">
+                <div class="row">
+                    <!-- Affiche concert -->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col s12 m12 l4">
+                                <img class="responsive-img materialboxed" src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/affiche.jpg">
+                            </div>
+                                <div class="col s12 m12 l4">
+                                    <p id="messageActuHV" class="white-text text-flow text-lighten-5">
+                                        Chers Amis et Admirateurs, je viens d'entreprendre ma tournée d'adieux à la chanson à travers quelques capitales du monde. Celle-ci m'emmènera à Mexico, Bogota, Pragues et quelques villes d'Europe et s'achèvera à Paris en Mai 2018.
+                                    </p>
+                                </div>
+                            <!--/div-->
+                            <!-- ici pour les postes -->
+                            <li>
+                                <p class="black-text btn waves-effect waves-teal btn light-green accent-3">Aller à la Boîte à messages...</p>
+                            </li>
+                            <div class="col s12 l4">
+                                <ul class="collapsible" data-collapsible="accordion">
+                                    <li>
+                                        <div class="collapsible-header  orange lighten-2">
+                                            <img src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/femme.jpg" alt="Avatar femme" class="circle responsive-img">
+                                            <div class="col s12">Ipsum</div>
+                                        </div>
+                                        <div class="collapsible-body white">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="collapsible-header  orange lighten-3">
+                                            <img src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/homme.jpg" alt="Avatar homme" class="circle responsive-img">
+                                            <div class="col s12">Ipsum</div>
+                                        </div>
+                                        <div class="collapsible-body white">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="collapsible-header  orange lighten-2">
+                                            <img src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/femme.jpg" alt="Avatar femme" class="circle responsive-img">
+                                            <div class="col s12">Ipsum</div>
+                                        </div>
+                                        <div class="collapsible-body white">
+                                            <p>Lorem ipsum dolor sit amet.</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="collapsible-header  orange lighten-3">
+                                            <img src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/homme.jpg" alt="Avatar homme" class="circle responsive-img">
+                                            <div class="col s12">Ipsum</div>
+                                        </div>
+                                        <div class="collapsible-body white">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="collapsible-header  orange lighten-2">
+                                            <img src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/femme.jpg" alt="Avatar femme" class="circle responsive-img">
+                                            <div class="col s12">Ipsum</div>
+                                        </div>
+                                        <div class="collapsible-body white">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        </div>
+                                    </li>
+                                </ul><!--/accordion-->
+                            <!--/div--><!--/col s12 l4-->
+                            </div><!--/col s12 l4-->
+                            
 
-
-    <!-- Zone Modifiée le 13/09/17 =========== -->
-
-    <!-- ACTUALITE PRINCIPALE -->
-    <div class="row">
-        <h2 class="white-text titreSection col s12 offset-l1">Actualités</h2>
-    </div>
-    <!--div class="container"-->
-    <div id="actualitePrincipale">
-        <div class="row">
-            <!-- Affiche concert -->
+                        </div><!--/row-->
+                    </div><!--/container-->
+                </div><!--/row-->
+            </div><!--/actualitePrincipale-->
             <div class="container">
                 <div class="row">
-                    <div class="col s12 m12 l4">
-                        <img class="responsive-img materialboxed" src="img/affiche.jpg">
-                    </div>
-                        <!-- Texte d'accroche -->
-                    <div class="row">
-                        <div class="col s12 m12 l4">
-                            <p class="white-text text-flow text-lighten-5">
-                                Chers Amis et Admirateurs, je viens d'entreprendre ma tournée d'adieux à la chanson à travers quelques capitales du monde. Celle-ci m'emmènera à Mexico, Bogota, Pragues et quelques villes d'Europe et s'achèvera à Paris en Mai 2018.
-                            </p>
+                    <div id="actualiteSuivante1">
+                        <div class="col s12 m6 l3">
+                            <img class="responsive-img materialboxed" src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/New1.jpg">
                         </div>
                     </div>
-                    <!-- ici pour les postes -->
+                    <div id="actualiteSuivante2">
+                        <div class="col s12 m6 l3">
+                            <img class="responsive-img materialboxed" src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/New2.jpg">
+                        </div>
+                    </div>
+                    <div id="actualiteSuivante3">
+                        <div class="col s12 m6 l3">
+                            <img class="responsive-img materialboxed" src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/New3.jpg">
+                        </div>
+                    </div>
+                    <div id="actualiteSuivante4">
+                        <div class="col s12 m6 l3">
+                            <img class="responsive-img materialboxed" src="http://localhost:8888/PiRoWorkHV/wp-content/uploads/2017/09/New4.jpg">
+                        </div>
+                    </div>
+                </div><!-- /'ROW' -->
+            </div>
+        </section><!--/section actualite-->
 
-
-                    <!-- Bouton 'Aller à la Boîte à messages' -->
-                    <li>
-                        <p class="black-text btn waves-effect waves-teal btn light-green accent-3">Aller à la Boîte à messages...</p>
-                    </li>
-                </div><!--/row-->
-            </div><!--/container-->
-            <!-- Liste des 5 derniers 'post' -->
-            <div class="container">
-                <div class="row">
-                    <div class="col s12 l12">
-                        <ul class="collapsible" data-collapsible="accordion">
-                            <li>
-                                <div class="collapsible-header  orange lighten-2">
-                                    <img src="img/avatars/femme.jpg" alt="Avatar femme" class="circle responsive-img">
-                                    <div class="col s12">Ipsum</div>
-                                </div>
-                                <div class="collapsible-body white">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header  orange lighten-3">
-                                    <img src="img/avatars/homme.jpg" alt="Avatar homme" class="circle responsive-img">
-                                    <div class="col s12">Ipsum</div>
-                                </div>
-                                <div class="collapsible-body white">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header  orange lighten-2">
-                                    <img src="img/avatars/femme.jpg" alt="Avatar femme" class="circle responsive-img">
-                                    <div class="col s12">Ipsum</div>
-                                </div>
-                                <div class="collapsible-body white">
-                                    <p>Lorem ipsum dolor sit amet.</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header  orange lighten-3">
-                                    <img src="img/avatars/homme.jpg" alt="Avatar homme" class="circle responsive-img">
-                                    <div class="col s12">Ipsum</div>
-                                </div>
-                                <div class="collapsible-body white">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header  orange lighten-2">
-                                    <img src="img/avatars/femme.jpg" alt="Avatar femme" class="circle responsive-img">
-                                    <div class="col s12">Ipsum</div>
-                                </div>
-                                <div class="collapsible-body white">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                </div>
-                            </li>
-                        </ul><!--/accordion-->
-                    </div><!--/col l6-->
-                </div>
-            </div><!--container-->
-        </div><!--/row-->
-    </div><!--/actualitePrincipale-->
-    <!-- ACTUALITES Suivantes -->
-    <div class="container">
-    <div class="row">
-    <!-- ACTUALITE Suivante 1 -->
-    <div id="actualiteSuivante1">
-    <!-- Illustration actualité 1 -->
-        <div class="col s12 m6 l3">
-            <img class="responsive-img materialboxed" src="http://localhost:8888/SiteHerve/wp-content/uploads/2017/09/New1.jpg">
-        </div>
-    </div>
-    <!-- ACTUALITE Suivante 2 -->
-    <div id="actualiteSuivante2">
-    <!-- Illustration actualité 2 -->
-        <div class="col s12 m6 l3">
-            <img class="responsive-img materialboxed" src="img/New2.jpg">
-        </div>
-    </div>
-    <!-- ACTUALITE Suivante 3 -->
-    <div id="actualiteSuivante3">
-        <!-- Illustration actualité 3 -->
-        <div class="col s12 m6 l3">
-            <img class="responsive-img materialboxed" src="img/New3.jpg">
-        </div>
-    </div>
-    <!-- ACTUALITE Suivante 4 -->
-    <div id="actualiteSuivante4">
-        <!-- Illustration actualité 4 -->
-        <div class="col s12 m6 l3">
-            <img class="responsive-img materialboxed" src="img/New4.jpg">
-        </div>
-    </div>
-    </div><!-- /'ROW' série 1 -->
-    <div class="row">
-    <!-- ACTUALITE Suivante 5 -->
-    <div id="actualiteSuivante5">
-    <!-- Illustration actualité 5 -->
-        <div class="col s12 m4 l4">
-            <img class="responsive-img materialboxed" src="img/0Accueil.png">
-        </div>
-    </div>
-    </div><!-- /'ROW' série 2 -->
-    </div>
-    
-    <!-- Fin de la zone modifiée ============= -->
-
-
-
-
-
+<!-- Fin de la zone modifiée ============= -->
 
 <?php
     include "footer.php";
