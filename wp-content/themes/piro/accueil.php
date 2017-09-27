a2773956e83352c9d2944870b83997374b3b5d4f
Pierrick LATIMIER <piefreemail@free.fr>
2017-09-27 12:15:00 +0200

    Modifs 'Structure-Accueil'
    - Changement de la Font
    - Suppression des Containers

--- wp-content/themes/piro/accueil.php
+++ wp-content/themes/piro/accueil.php
@@ -3,17 +3,15 @@
    include "header_home.php";
 ?>
 
-
     <!-- Section 'Biographie' -->
-    <section id="biographie" class="scrollspy">    <!-- template-page-aPropos amber lighten-5"-->
+    <section id="biographie" class="scrollspy">
         <h2 class="flow-text green-text">BIOGRAPHIE</h2>
         <div class="row">
             <div class="card horizontal hoverable">
-                <div class="card-stacked">
+                <div class="card-stacked"-->
                     <div class="card-content">
-                        <p class="flow-text  col l12">Fils de Marcelle Blanche Villard une vendeuse de violettes et de partitions qui chante dans les rues avant d'être ouvreuse au théâtre des Variétés, le petit René Paul Hervé naît dans un taxi alors que sa mère se rend à l'hôpital Saint-Antoine.</p>
-                            <img class="materialboxed col l2" src="wp-content/uploads/2017/09/2.jpg">
-                        <p class="flow-text">Très tôt, Hervé est envoyé à l'orphelinat Saint-Vincent-de-Paul, situé à Paris. Séparé de sa famille, il tente de s'échapper plusieurs fois. En 1957, à l'âge de 11 ans, il rencontre à La Celette près de Saint-Amand-Montrond celui qui sera son père spirituel et qui lui donnera une solide formation scolaire, l'abbé Denis Angrand.  À 13 ans, Hervé obtient son certificat d'études et décide, à 14 ans, que la musique sera son avenir.</p>
+                        <img class="materialboxed col l2" src="wp-content/uploads/2017/09/2.jpg">
+                        <p class="flow-text green-text col l12">"...à 14 ans, j'ai décidé que la musique serait mon avenir..."
                     </div>
                     <div class="card-action">
                         <a class="black-text right" href="a-propos">Lire la suite</a>
@@ -25,7 +23,6 @@
             </div>
         </div>
     </section>
-
 
     <!-- Section Actualités -->
     <section id="actualites" class="scrollspy">
@@ -58,7 +55,6 @@
             </div>
         </div>
     </section>
-
 
     <!-- Section Littérature -->
     <section id="litterature" class="scrollspy">
@@ -101,10 +97,9 @@
         </div>
     </section>
 
-
     <!-- Section Discographie -->
-    <section id="discographie" class="scrollspy container">
-        <h2 class="flow-text green-text">Discographie</h2>
+    <section id="discographie" class="scrollspy">
+        <h2 class="flow-text green-text">DISCOGRAPHIE</h2>
         <div class="container row">
             <div class="parallax-container">
                 <div class="row">
@@ -120,49 +115,40 @@
             </div>
         </div>
     </section>
-    
 
     <!-- Section Galeries -->
-    <section id="galeries" class="scrollspy container">
-        <div class="section">
-            <h2 class="flow-text white-text">Galeries</h2>
-        </div>
-    </section>
-    <div class="parallax-container">
-        <div class="parallax">
-            <img src="wp-content/uploads/2017/09/DSC05255.png">
-        </div>
-    </div>
-    <div class="divider"></div>
-
-
-
-    <!-- Section Contact -->
-    <section id="contact" class="scrollspy container">
-        <div class="section">
-            <h2 class="flow-text white-text">Contact</h2>
-        </div>
-    </section>
-    <div class="container row">
-        <img class="responsive-img col l9" src="wp-content/uploads/2017/09/IMG_9851-2.png" alt="img_9851-2.png">
-        <div class="flow-text white-text col l3">
-            <p class="flow-text white-text">N'hésitez pas à me laisser un message !</p>
-            <p class="flow-text white-text">Je vous répondrai dès que possible.</p>
-            <p class="flow-text white-text">Bien à vous. Hervé</p>
-        </div>
-        <div class="row">
-            <div class="col l12">
-                <a href="contact">
-                <button class="btn waves-effect waves-light light-green accent-3 valign right black-text" type="submit" name="action">C'est par ici !<i class="material-icons right">chevron_right</i>
-                </button></a>
+    <section id="galeries" class="scrollspy">
+        <h2 class="flow-text green-text">GALERIES</h2>
+        <div class="parallax-container">
+            <div class="parallax">
+                <img src="wp-content/uploads/2017/09/DSC05255.png">
             </div>
         </div>
-    </div>
-    <div class="divider"></div>
+    </section>
 
+    <!-- Section Contact -->
+    <section id="contact" class="scrollspy">
+        <h2 class="flow-text green-text">CONTACT</h2>
+        <div class="container row">
+            <img class="responsive-img col l9" src="wp-content/uploads/2017/09/IMG_9851-2.png" alt="img_9851-2.png">
+            <div class="flow-text white-text col l3">
+                <p class="flow-text white-text">N'hésitez pas à me laisser un message !</p>
+                <p class="flow-text white-text">Je vous répondrai dès que possible.</p>
+                <p class="flow-text white-text">Bien à vous. Hervé</p>
+            </div>
+            <div class="row">
+                <div class="col l12">
+                    <a href="contact">
+                        <button class="btn waves-effect waves-light light-green accent-3 valign right black-text" type="submit" name="action">C'est par ici !<i class="material-icons right">chevron_right</i>
+                    </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+    </section>
   
     <!-- FLOAT NAV RIGHT -->
-
     <div id="rightNavFloat" style="height: 1px;" class="hide-on-small-only">
         <ul class="section table-of-contents ">
             <li><a class="white-text"href="#biographie">Biographie</a></li>
@@ -175,7 +161,6 @@
     </div>
 
     <!-- FIN FLOAT NAV RIGHT -->
-
     <div id="goToTop">
 		<a id="goToTopLink" href="#biographie"><i class="medium material-icons">expand_less</i></a>
 	</div>
